df0a5c91f617fdb41ddc1b5fa31e69927d3d7392
MSP1981 <99791179+MSP1981@users.noreply.github.com>
2022-12-19 15:06:20 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] fibonacci = new int[20];
+        for (int i = 0; i < fibonacci.length; i++) {
+            if (i == 0 || i == 1) {
+                fibonacci[i] = i;
+            } else {
+                fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+            }
+        }
+        System.out.println(Arrays.toString(fibonacci));
+    }
+}
